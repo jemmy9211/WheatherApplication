8824ae290a238601a53bc2dc29e2b78c089439eb
jemmy9211 <jemmy9211@gmail.com>
2023-07-13 16:47:12 +0800

    no message

--- data.js
+++ data.js
@@ -12,7 +12,6 @@ var flag=0;
 var rnum=0;
 var glbalres;
 mainstream(lacationnum);
-nowweather(lacationnum);
 document.getElementById('list').onchange = function() {
     if(this.value=='臺中市'){
         lacationnum=20;
@@ -35,9 +34,9 @@ document.getElementById('list').onchange = function() {
     }
     console.log(lacationnum);
     mainstream(lacationnum);
-    nowweather(lacationnum);
 }
 function mainstream(lacationnum){
+    nowweather(lacationnum);
     $.ajax({
     url:'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-091?Authorization=CWB-373C6328-6BF2-41B3-BB3B-147802B82875&format=JSON&locationName=&elementName=&sort=time',
     method: "GET",
